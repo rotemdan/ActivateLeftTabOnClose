8145249dfe3a4d375041b4b69cedca694f52c6a4
Rotem Dan <rotemdan@gmail.com>
2019-08-09 12:25:55 +0300

    Update successorTabId property if available

--- background.js
+++ background.js
@@ -1,11 +1,34 @@
 let tabStateSnapshot = [];
 
-async function updateTabStateSnapshot() {
+async function onTabsUpdated() {
 	tabStateSnapshot = await browser.tabs.query({ windowType: "normal" });
+
+	if (typeof browser.tabs.moveInSuccession === 'function') {
+		const currentTabInfo = tabStateSnapshot.filter((tabInfo) => {
+			return tabInfo.active === true
+		})[0];
+
+		if (!currentTabInfo) {
+			return;
+		}
+
+		const tabToTheLeftInfo = tabStateSnapshot.filter((tabInfo) => {
+			return tabInfo.windowId == currentTabInfo.windowId &&
+				tabInfo.index == currentTabInfo.index - 1;
+		})[0];
+
+		if (!tabToTheLeftInfo) {
+			return;
+		}
+
+		if (currentTabInfo.successorTabId !== tabToTheLeftInfo.id) {
+			await browser.tabs.update(currentTabInfo.id, { successorTabId: tabToTheLeftInfo.id });
+		}
+	}
 }
 
-async function onRemoved(removedTabId, removeInfo) {
-	log("onRemoved:", removedTabId, removeInfo);
+async function onTabRemoved(removedTabId, removeInfo) {
+	log("onTabRemoved:", removedTabId, removeInfo);
 
 	const matchingTabInfo = tabStateSnapshot.filter((tabInfo) => {
 		return tabInfo.active == true &&
@@ -19,29 +42,28 @@ async function onRemoved(removedTabId, removeInfo) {
 
 		const tabToTheLeftInfo = tabStateSnapshot.filter((tabInfo) => {
 			return tabInfo.windowId == matchingTabInfo.windowId &&
-				   tabInfo.index == matchingTabInfo.index - 1;
+				tabInfo.index == matchingTabInfo.index - 1;
 		})[0];
 
 		if (tabToTheLeftInfo) {
-			await browser.tabs.update(tabToTheLeftInfo.id, { active: true });
-			log(`Activated tab ${tabToTheLeftInfo.id} in window ${tabToTheLeftInfo.windowId}.`);
+			if (tabToTheLeftInfo.active === false) {
+				await browser.tabs.update(tabToTheLeftInfo.id, { active: true });
+				log(`Activated tab ${tabToTheLeftInfo.id} in window ${tabToTheLeftInfo.windowId}.`);
+			}
 		} else {
 			log(`No matching tab to the left was found.`);
 		}
 	}
 
-	await updateTabStateSnapshot();
+	await onTabsUpdated();
 }
 
-browser.tabs.onActivated.addListener(updateTabStateSnapshot);
-browser.tabs.onAttached.addListener(updateTabStateSnapshot);
-browser.tabs.onCreated.addListener(updateTabStateSnapshot);
-browser.tabs.onDetached.addListener(updateTabStateSnapshot);
-browser.tabs.onHighlighted.addListener(updateTabStateSnapshot);
-browser.tabs.onMoved.addListener(updateTabStateSnapshot);
-browser.tabs.onRemoved.addListener(onRemoved);
-browser.tabs.onReplaced.addListener(updateTabStateSnapshot);
-browser.tabs.onUpdated.addListener(updateTabStateSnapshot);
-//browser.tabs.onZoomChange.addListener(updateTabStateSnapshot);
-
-setInterval(updateTabStateSnapshot, 100);
+browser.tabs.onActivated.addListener(onTabsUpdated);
+browser.tabs.onAttached.addListener(onTabsUpdated);
+browser.tabs.onCreated.addListener(onTabsUpdated);
+browser.tabs.onDetached.addListener(onTabsUpdated);
+browser.tabs.onHighlighted.addListener(onTabsUpdated);
+browser.tabs.onMoved.addListener(onTabsUpdated);
+browser.tabs.onRemoved.addListener(onTabRemoved);
+browser.tabs.onReplaced.addListener(onTabsUpdated);
+browser.tabs.onUpdated.addListener(onTabsUpdated);
